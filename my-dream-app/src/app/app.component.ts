a9d0b5a9767e2c19fdf35ef0755dc3bcf64a6642
LeoMotoMoto200 <lbacac@unsa.edu.pe>
2025-07-02 08:08:38 -0500

    Se modifica el app.module.ts para corregir el error del showHobbies.

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'my-dream-app';
   name : string;
   email : string;
   webpage : string;
   hobbies : string[];
+  showHobbies : boolean = true;
 
   constructor() {
     console.log("Constructor working... ");
@@ -18,22 +25,19 @@ export class AppComponent {
     this.email = "ccorrales@unsa.edu.pe";
     this.webpage = "http://www.unsa.edu.pe";
     this.hobbies = ["Futbol", "Programacion", "Netflix"];
+    this.showHobbies = true;
   }
 
   toggleHobbies() {
-    this.showhobbies = !this.showhobbies;
+    this.showHobbies = !this.showHobbies;
   }
 
-  newHobby(hobby: HTMLInputElement) {
+  newHobby(hobby: any) {
     if (hobby.value.trim()) { // .trim() para evitar hobbies vacíos
         this.hobbies.push(hobby.value.trim());
         hobby.value = '';
     }
     hobby.focus();
     return false;
-  }
-
-  showhobbies() {
-    return true;
   }
 }
