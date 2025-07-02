55607d405ec272bf61c842df29813479ecbc9247
LeoMotoMoto200 <lbacac@unsa.edu.pe>
2025-07-02 07:36:31 -0500

    Agregando Constructor y usando For e Ifs para la ejecución.

---
+++ my-dream-app/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-dream-app';
+  name : string;
+  email : string;
+  webpage : string;
+  hobbies : string[];
+
+  constructor() {
+    console.log("Constructor working... ");
+    this.name = "Carlo Jose Luis";
+    this.email = "ccorrales@unsa.edu.pe";
+    this.webpage = "http://www.unsa.edu.pe";
+    this.hobbies = ["Futbol", "Programacion", "Netflix"];
+  }
+
+  showhobbies() {
+    return true;
+  }
+}
