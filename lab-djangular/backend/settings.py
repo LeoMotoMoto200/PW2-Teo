21ff45b5f8dfceee217a96248b9ce18a8ab336e3
LeoMotoMoto200 <lbacac@unsa.edu.pe>
2025-07-09 08:13:40 -0500

    Integra la aplicación Angular compilada en Django.

--- lab-djangular/backend/settings.py
+++ lab-djangular/backend/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, '../frontend/dist/frontend/browser')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# backend/settings.py
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '../frontend/dist/frontend/browser')
+]
