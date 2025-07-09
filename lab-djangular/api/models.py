add562873e9d66d224970641de0afdc95d452f79
LeoMotoMoto200 <lbacac@unsa.edu.pe>
2025-07-09 07:42:28 -0500

    Crea API simple para Tareas.

--- lab-djangular/api/models.py
+++ lab-djangular/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Tarea(models.Model):
+    titulo = models.CharField(max_length=200)
+    completada = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.titulo
+
